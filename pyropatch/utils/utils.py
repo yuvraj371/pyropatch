ae3a28b46f22fe4976778e76de6026f774f6f16e
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2024-02-14 19:39:35 +0530

    Update utils.py

--- pyropatch/utils/utils.py
+++ pyropatch/utils/utils.py
@@ -103,3 +103,19 @@ def get_input_file_from_file_id(
         )
 
     raise ValueError(f"Unknown file id: {file_id}")
+
+
+def unpack_new_file_id(new_file_id):
+    """Return file_id, file_ref"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "  <  iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
